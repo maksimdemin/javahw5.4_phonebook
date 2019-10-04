3041fb2c24469056cf9892b29ee35ed82ab9a6a0
MaksimDemin <mdeminalex@icloud.com>
2019-10-05 03:02:41 +0800

    change all project

--- src/Main.java
+++ src/Main.java
@@ -4,46 +4,52 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+
         PhoneBook myPhoneBook = new PhoneBook(); // создаем объект класса PhoneBook
+
+        System.out.println(myPhoneBook.HELP);
 
         for (;;) {
             Scanner scan = new Scanner(System.in);
-            System.out.println("Enter Name or phonenumber");
+            System.out.println("Enter Name/phonenumber or command (for help enter command HELP).");
             String enterData = scan.nextLine();
 
-            if (myPhoneBook.phoneBook.containsKey(enterData)) {
+            if (myPhoneBook.isContainsKey(enterData)) {
                 System.out.println("This contact already exists.");
                 myPhoneBook.printInfoAboutContactForKey(enterData);
             }
-            else if (myPhoneBook.phoneBook.containsValue(enterData)) {
+            else if (myPhoneBook.isContainsValue(enterData)) {
                 System.out.println("This contact already exists.");
                 myPhoneBook.printInfoAboutContactForValue(enterData);
             }
-            else if (!myPhoneBook.phoneBook.containsValue(enterData) && !myPhoneBook.phoneBook.containsKey(enterData) && !enterData.equals("LIST")) {
-                if (enterData.matches("\\D+")) {
-                    System.out.println("This contact does not exist. You have entered a name.");
-                    System.out.println("Enter phonenumber for create new contact.");
-                    String enterNewNumber = scan.nextLine();
-                    if (enterNewNumber.matches("\\+?\\d+")) {
-                        myPhoneBook.addToMap(enterNewNumber, enterData);
-                        System.out.println("A new contact has been added to your phonebook successfully.");
-                        //myPhoneBook.printMap();
-                    } else System.out.println("Invalid number. Try again.");
-                }
-                else if (enterData.matches("\\+?\\d+")) {
-                    System.out.println("This contact does not exist. You have entered a number.");
-                    System.out.println("Enter name for create new contact.");
-                    String enterNewName = scan.nextLine();
-                    if (enterNewName.matches("\\D+")) {
-                        myPhoneBook.addToMap(enterData, enterNewName);
-                        System.out.println("A new contact has been added to your phonebook successfully.");
-                        //myPhoneBook.printMap();
-                    } else System.out.println("Invalid name. Try again.");
-                }
+            else if (!myPhoneBook.isContainsValue(enterData) && !myPhoneBook.isContainsKey(enterData) && !enterData.toUpperCase().matches("LIST|CLEAR|UPDATE|DELETE|HELP|EXIT")) {
+                myPhoneBook.addToMap(enterData);
             }
-            else if (enterData.equals("LIST")) {
+            else if (enterData.toUpperCase().matches("LIST")) {
+                if (myPhoneBook.isEmptyMap()) {
+                    System.out.println("Your phonebook is empty. Create new phonebook.");
+                }
                 myPhoneBook.printMap();
             }
+            else if (enterData.toUpperCase().matches("CLEAR")) {
+                myPhoneBook.deleteAllContacts();
+                System.out.println("Your phonebook is empty. Create new phonebook or enter EXIT.");
+            }
+            else if (enterData.toUpperCase().matches("UPDATE")) {
+                myPhoneBook.updateContact();
+            }
+            else if (enterData.toUpperCase().matches("DELETE")) {
+                myPhoneBook.deleteContact();
+            }
+            else if (enterData.toUpperCase().matches("EXIT")) {
+                System.out.println("Goodbye");
+                break;
+            }
+            else if (enterData.toUpperCase().matches("HELP")) {
+                System.out.println(myPhoneBook.HELP);
+            }
+            else System.out.println("Invalid number, name or command. Try again (for help enter command HELP).");
         }
     }
 }
--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -2,11 +2,86 @@ import java.util.*;
 
 public class PhoneBook {
 
-    public HashMap<String, String > phoneBook = new HashMap<>(); // создаем объект класса HashMap. В этом "словаре" будем хранить телефонную книгу
+    private final String REGEX_NAME = "(.*(\\d\\D|\\D\\d).*|\\D+)";
+    private final String REGEX_PHONE = "(\\+?\\d?\\s?-?\\(?\\d{3}\\)?\\s?-?\\d{3}\\s?-?\\d{2}\\s?-?\\d{2})";
+    private static final String HELP_FOR_NUMBER = "The phonenumber should contain only 10 digits (not including +7/8)\n" +
+            "for example: <(495)-111-11-11> or <4951111111>\n" +
+            "or 11 digits (including +7/8)\n" +
+            "for example: <+7(495)-111-11-11> or <84951111111>";
+    public static final String HELP = "\n" + HELP_FOR_NUMBER  +
+            "\nThe contact name should not contain only numbers.\n" +
+            "You can use all characters except <-> (minus), <()> (brackets), <+> (plus).\n\n" +
+            "Available commands:\n" +
+            "LIST - show all posts phonebook.\n" +
+            "CLEAR - delete all contacts.\n" +
+            "UPDATE - change contact.\n" +
+            "DELETE - delete one of the contacts.\n" +
+            "HELP - help section.\n" +
+            "EXIT - stop program";
 
-    public void addToMap(String enterNewNumber, String enterData) { // метод для добавления пары (ключ, значение) в множество
-        phoneBook.put(enterNewNumber, enterData);
+
+    private TreeMap<String, String > phoneBook = new TreeMap<>(); // создаем объект класса TreeMap. В этом "словаре" будем хранить телефонную книгу
+
+
+    //-------------------------------------CREATE-----------------------------------------------------------------------
+
+    public void addToMap(String enterData) { // метод для добавления пары (ключ, значение) в множество
+        if (enterData.matches(REGEX_NAME) && enterData.matches("[^+()-]+")) { // проверка введенной строки на имя
+            System.out.println("This contact does not exist. You have entered a name.\n" +
+                    "Enter phonenumber for create new contact.");
+            System.out.println(HELP_FOR_NUMBER);
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                String enterNewNumber = scanner.nextLine();
+                if (enterNewNumber.matches(REGEX_PHONE)) {
+
+                    enterNewNumber = enterNewNumber.replaceAll("[^0-9]", "");
+                    if (enterNewNumber.matches("\\d{10}")) {
+                        String regionRus = "+7";
+                        enterNewNumber = regionRus.concat(enterNewNumber);
+                        phoneBook.put(enterNewNumber, enterData);
+                        System.out.println("A new contact has been added to your phonebook successfully.");
+                        return;
+                    } else if (enterNewNumber.matches("\\d{11}")) {
+                        enterNewNumber = enterNewNumber.replaceAll("^\\d", "+7");
+                        phoneBook.put(enterNewNumber, enterData);
+                        System.out.println("A new contact has been added to your phonebook successfully.");
+                        return;
+                    }
+                } else System.out.println("Invalid number. Try again. " + HELP_FOR_NUMBER);
+            }
+        } else if (enterData.matches(REGEX_PHONE)) {
+
+            System.out.println("This contact does not exist. You have entered a number.");
+            System.out.println("Enter name for create new contact. Important! The contact name should not contain only numbers.");
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                String enterNewName = scanner.nextLine();
+                if (enterNewName.matches(REGEX_NAME) && enterNewName.matches("[^+()-]+") && !enterNewName.toUpperCase().matches("LIST|CLEAR|UPDATE|DELETE|HELP")) {
+                    phoneBook.put(validationNumber(enterData), enterNewName);
+                    System.out.println("A new contact has been added to your phonebook successfully.");
+                    return;
+                } else System.out.println("Invalid name. Try again. Important! The contact name should not contain only numbers.");
+            }
+        } else System.out.println("Invalid number, name or command. Try again.");
     }
+
+    //--------------------------------------READ------------------------------------------------------------------------
+
+    public void printInfoAboutContactForKey(String enterData) { // метода для вывода информации о контакте, если совпадает номер контакта
+        System.out.printf("Name: %s - phonenumber: %s\n", phoneBook.get(validationNumber(enterData)), validationNumber(enterData));
+    }
+
+
+    public void printInfoAboutContactForValue(String enterData) { // метода для вывода информации о контакте, если совпадает имя контакта
+        Set<String> keys = phoneBook.keySet();
+        for (String number: keys) {
+            if (phoneBook.get(number).equals(enterData)) {
+                System.out.printf("Name: %s - phonenumber: %s\n", enterData, number);
+            }
+        }
+    }
+
 
     public void printMap() { // метод, который выводит всю карту в консоль
         for (Map.Entry<String, String> map: phoneBook.entrySet())
@@ -15,19 +90,139 @@ public class PhoneBook {
         }
     }
 
-    public void printInfoAboutContactForKey(String enterData) { // метода для вывода информации о контакте, если совпадает номер контакта
-                System.out.printf("Name: %s - phonenumber: %s\n", phoneBook.get(enterData), enterData);
+
+    public boolean isContainsKey(String enterData) { // метод для проверки наличия в книге номера, также проверка на валидность введенных данных
+        boolean isContains = false;
+        if (enterData.matches(REGEX_PHONE)) {
+            enterData = enterData.replaceAll("[^0-9]", "");
+            if (enterData.matches("^\\d+") && enterData.matches("\\d{10}")) {
+                String regionRus = "+7";
+                enterData = regionRus.concat(enterData);
+                if (phoneBook.containsKey(enterData)) {
+                    isContains = true;
+                }
             }
-
-
-    public void printInfoAboutContactForValue(String enterData) { // метода для вывода информации о контакте, если совпадает имя контакта
-        Set<String> keys = phoneBook.keySet();
-        for (String i: keys) {
-            if (phoneBook.get(i).equals(enterData)) {
-                String findKey = i;
-                System.out.printf("Name: %s - phonenumber: %s\n", enterData, findKey);
+            else if (enterData.matches("\\d{11}")) {
+                enterData = enterData.replaceAll("^\\d", "+7");
+                if (phoneBook.containsKey(enterData)) {
+                    isContains = true;
+                }
             }
         }
+        return isContains;
+    }
+
+
+    public boolean isContainsValue(String enterData) { // метод для проверки наличия в книге имени, также проверка на валидность введенных данных
+        boolean isContains = false;
+        if (phoneBook.containsValue(enterData) && enterData.matches(REGEX_NAME) && enterData.matches("[^+()-]+")) {
+            isContains = true;
+        }
+        return isContains;
+    }
+
+    public boolean isEmptyMap() { // метод для проверки пустое множество или нет
+        boolean isEmpty = false;
+        if (phoneBook.isEmpty()) {
+            isEmpty = true;
+        }
+        return isEmpty;
+    }
+
+    private String validationNumber(String enterData) { // валидация введенного номера в иных случаях
+        enterData = enterData.replaceAll("[^0-9]", "");
+        if (enterData.matches("^\\d+") && enterData.matches("\\d{10}")) {
+            String regionRus = "+7";
+            enterData = regionRus.concat(enterData);
+        } else if (enterData.matches("\\d{11}")) {
+            enterData = enterData.replaceAll("^\\d", "+7");
+        }
+        return enterData;
+    }
+
+    private String validatonTenDigitsNumber(String enterData) {
+        enterData = enterData.replaceAll("[^0-9]", "");
+        if (enterData.matches("^\\d+") && enterData.matches("\\d{10}")) {
+            String regionRus = "+7";
+            enterData = regionRus.concat(enterData);
+        }
+        return enterData;
+    }
+
+    //---------------------------------------------UPDATE---------------------------------------------------------------
+
+    public void updateContact() {
+        if (isEmptyMap()) {
+            System.out.println("Your phonebook is empty. Create new phonebook.");
+        }
+        else
+            while (true) {
+            System.out.println("Enter the number or name of the contact to be modified.");
+        Scanner scanner = new Scanner(System.in);
+        String enterData = scanner.nextLine();
+         if (isContainsKey(enterData)) {
+             System.out.println("This contact will be changed.");
+             printInfoAboutContactForKey(validationNumber(enterData));
+             System.out.println("Enter new phonenumber.");
+
+             while (true) {
+                 String enterUpdateNumber = scanner.nextLine();
+                 if (enterUpdateNumber.matches(REGEX_PHONE)) {
+                     enterUpdateNumber = enterUpdateNumber.replaceAll("[^0-9]", "");
+                     if (enterUpdateNumber.matches("^\\d+") && enterUpdateNumber.matches("\\d{10}")) {
+                         String regionRus = "+7";
+                         enterUpdateNumber = regionRus.concat(enterUpdateNumber);
+                         String oldNumber = validationNumber(enterData);
+                         phoneBook.put(enterUpdateNumber, phoneBook.get(validationNumber(enterData)));
+                         phoneBook.remove(oldNumber);
+                         System.out.println("Your contact has been updated successfully.");
+                         return;
+                     } else if (enterUpdateNumber.matches("\\d{11}")) {
+                         enterUpdateNumber = enterUpdateNumber.replaceAll("^\\d", "+7");
+                         String oldNumber = validationNumber(enterData);
+                         phoneBook.put(enterUpdateNumber, phoneBook.get(validationNumber(enterData)));
+                         phoneBook.remove(oldNumber);
+                         System.out.println("Your contact has been updated successfully.");
+                         return;
+                     }
+                 }
+                 else System.out.println("Invalid phonenumber. Try again. " + HELP_FOR_NUMBER);
+             }
+         }
+         else System.out.println("Phonenumber not found in your phone book. Try again.");
+            }
+            System.out.println("Invalid phonenumber, name or command. Try again.");
+    }
+
+    //----------------------------------------------DELETE--------------------------------------------------------------
+
+    public void deleteContact() { // метод, который удаляет контакт по номеру
+        if (isEmptyMap()) {
+            System.out.println("Your phonebook is empty. Create new phonebook.");
+        } else
+            while (true)
+            {
+                System.out.println("Enter the phonenumber to delete this contact.");
+                Scanner scanner = new Scanner(System.in);
+                String enterData = scanner.nextLine();
+
+                if (isContainsKey(enterData)) {
+                      String deletedNumber = validationNumber(enterData);
+                    System.out.printf("Name: %s - phonenumber: %s\n", phoneBook.get(validationNumber(enterData)), deletedNumber +" <- this contact deleted successfully.");
+                    phoneBook.remove(validationNumber(enterData));
+                    return;
+                }
+                else System.out.println("Phonenumber not found in your phonebook. Try again.. " + HELP_FOR_NUMBER);
+            }
+            System.out.println("Foooooo.");
+    }
+
+
+    public void deleteAllContacts() { // метод, который удаляет всё множество полностью
+        if (isEmptyMap()) {
+            System.out.println("Cannot empty an empty phone book.");
+        }
+        phoneBook.clear();
     }
 }
 
